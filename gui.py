f73a7fb8743a9ea5eb56016eb0f948d619315093
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-04-01 22:42:07 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -14,13 +14,53 @@ class GameWindow:
         self.game2_button = tk.Button(parent, text="Game 2", command=self.run_game2)
         self.game2_button.pack(side="right", padx=10)
 
+        # Create an Entry widget for entering player name
+        self.player_name_label = tk.Label(parent, text="Enter player name:")
+        self.player_name_label.pack()
+
+        self.player_name_entry = tk.Entry(parent)
+        self.player_name_entry.pack()
+
+        # Create a button for starting the game with the entered player name
+        self.start_game_button = tk.Button(parent, text="Start Game", command=self.start_game)
+        self.start_game_button.pack(pady=10)
+
+        # Create a label for displaying the selected game
+        self.selected_game_label = tk.Label(parent, text="")
+        self.selected_game_label.pack()
+
+        # Initialize a variable for storing the selected game
+        self.selected_game = ""
+
     def run_game1(self):
-        # Run game 1
-        os.system("python C:/Users/User/Documents/GitHub/pygame/flappy.py")
+        # Set the selected game to game 1
+        self.selected_game = "game1"
+
+        # Update the selected game label
+        self.selected_game_label.config(text="Selected game: Game 1")
 
     def run_game2(self):
-        # Run game 2
-        os.system("python C:/Users/User/Documents/GitHub/pygame/car-race-python-game-master/game.py")
+        # Set the selected game to game 2
+        self.selected_game = "game2"
+
+        # Update the selected game label
+        self.selected_game_label.config(text="Selected game: Game 2")
+
+    def start_game(self):
+        # Get the entered player name
+        player_name = self.player_name_entry.get()
+
+        # Check if a game has been selected and a player name has been entered
+        if self.selected_game != "" and player_name != "":
+            # Run the selected game with the player name as an argument
+            if self.selected_game == "game1":
+                os.system(f"python C:/Users/User/Documents/GitHub/pygame/flappy.py {player_name}")
+            elif self.selected_game == "game2":
+                os.system(f"python C:/Users/User/Documents/GitHub/pygame/car-race-python-game-master/game.py {player_name}")
+        else:
+            # Display an error message if a game has not been selected or a player name has not been entered
+            error_message = "Please select a game and enter a player name."
+            self.selected_game_label.config(text=error_message)
 
 if __name__ == "__main__":
     root = tk.Tk()
